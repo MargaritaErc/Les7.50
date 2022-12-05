b538542beb37df2106cabb75c5f95362522223aa
m.rita1650@gmail.com <m.rita1650@gmail.com>
2022-12-05 13:38:35 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+int[,] matrix = new int [new Random().Next(3,5),new Random().Next(3,5)];
+for (int i= 0; i< matrix.GetLength(0); i++)
+{
+   for (int j = 0; j < matrix.GetLength(1); j++)
+   {
+    matrix [i,j] = new Random().Next(0,12);
+    Console.WriteLine(matrix[i,j] + "");
+   }
+Console.WriteLine();
+}
+Console.WriteLine("_________");
+
+double sum = 0;
+for (int j=0; j<matrix.GetLength(1); j++)
+{
+    sum+= matrix [i,j];
+}
+double average = Math.Round(sum/matrix.GetLength(0),2);
+sum = 0;
+Console.Write(average + " ");
